1d0c78baee4fca9a8cfbad4895577e16a02c9ba6
zenofirminx-pixel <zenofirminx@gmail.com>
2026-06-10 05:45:16 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "status": "online",
+        "name": "AurX API"
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860)
